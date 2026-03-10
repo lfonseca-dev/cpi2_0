1f782221e322d7c1af233dd9192c901ce2a6544a
AmorimDeveloper <gabriel.amorim.etec2@gmail.com>
2026-03-10 02:24:11 +0000

    Inicio da navbar

--- APP/src/components/NavBar.jsx
+++ APP/src/components/NavBar.jsx
@@ -1,28 +1,32 @@
-import { AiFillHome } from "react-icons/ai";
-import { CgProfile } from "react-icons/cg";
-import { FaGear, FaPhone } from "react-icons/fa6";
-
+import { FaIndustry } from "react-icons/fa";
+import { FaMagnifyingGlass, FaCirclePlus } from "react-icons/fa6";
+import Logo from "../assets/LogoFaulin.png";
 
 function NavBar() {
     return ( 
-        <header className="flex justify-around items-center bg-white p-4 shadow-xl h-[13vh]">
-            <img src="https://storage.googleapis.com/ecdt-logo-saida/dd4a6c12ed34d5a71b03c3e90fc33f4ed8567f53f886f5b3c38213d0f619561b/CERAMICA-FAULIN-LIMITADA.webp" alt="" 
-            className="h-full aspect-square object-contain hover:scale-[1.2]"/>
-            <ul className="flex justify-around items-center text-[30px] font-bold w-[50%]">
-                <li className="flex items-center gap-[10px]">
-                    <AiFillHome />
-                    <span>Home</span>
+        <header className="flex items-center justify-between bg-white p-4 h-[13vh] shadow">
+            <span className="flex items-center justify-center h-[145%] w-[12%]">
+                <img src={Logo} alt="" 
+                className="h-full aspect-square object-contain duration-[.3s] hover:scale-[1.2]"/>
+            </span>
+            <ul className="flex items-center justify-between text-[16px] font-bold w-[35%] h-full">
+                <li className="link">
+                    <FaMagnifyingGlass />
+                    <span>Consultar</span>
                 </li>
-                <li className="flex items-center gap-[10px]">
-                    <CgProfile />
-                    <span>Perfil</span>
+                <li className="link">
+                    <FaCirclePlus />
+                    <span>Cadastro</span>
                 </li>
-                <li className="flex items-center gap-[10px]">
-                    <FaPhone />
-                    <span>Contato</span>
+                <li className="link">
+                    <FaIndustry />
+                    <span>Produção</span>
                 </li>
             </ul>
-            <FaGear className="text-[30px]" />
+            <span className="flex items-center justify-center h-full w-[12%]">
+                <img src="https://thumbs.dreamstime.com/b/%C3%ADcone-de-perfil-do-avatar-padr%C3%A3o-vetor-m%C3%ADdia-social-usu%C3%A1rio-retrato-176256935.jpg" alt="" 
+                className="h-full aspect-square object-contain bg-[gray] duration-[.3s] rounded-[50rem] border-[2px] border-[gray] hover:scale-[1.2]"/>
+            </span>
         </header>
     );
 }
