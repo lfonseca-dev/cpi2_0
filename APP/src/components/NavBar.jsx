dc2de89b274f3c1210820d7ea2f8d4af2bab4d91
AmorimDeveloper <gabriel.amorim.etec2@gmail.com>
2026-03-12 17:24:52 +0000

    Atualizando a navbar

--- APP/src/components/NavBar.jsx
+++ APP/src/components/NavBar.jsx
@@ -4,19 +4,32 @@ import Logo from "../assets/LogoFaulin.png";
 
 function NavBar() {
     return ( 
-        <header className="flex items-center justify-between bg-white p-4 h-[13vh] shadow">
+        <header className="flex items-center justify-center fixed bg-white p-4 h-[13vh] w-screen top-0 shadow">
             <span className="flex items-center justify-center h-[145%] w-[12%]">
                 <img src={Logo} alt="" 
                 className="h-full aspect-square object-contain duration-[.3s] hover:scale-[1.2]"/>
             </span>
-            <ul className="flex items-center justify-between text-[16px] font-bold w-[35%] h-full">
+            <ul className="flex items-center justify-evenly text-[16px] font-bold w-[76%] h-full">
                 <li className="link">
                     <FaMagnifyingGlass />
                     <span>Consultar</span>
                 </li>
-                <li className="link">
+                <li className="link cadastro h-full">
                     <FaCirclePlus />
                     <span>Cadastro</span>
+                    <div className="flex justify-center absolute bg-neutral-800 h-[270px] bottom-[-270px] text-white p-4">
+                        <ul className="flex flex-col justify-between items-center">
+                            <li className="link">user</li>
+                            <li className="link">categoria</li>
+                            <li className="link">produto</li>
+                            <li className="link">fornecedor</li>
+                            <li className="link">operador</li>
+                            <li className="link">engenheiro</li>
+                            <li className="link">responsavel</li>
+                            <li className="link">destino</li>
+                            <li className="link">local_estoque</li>
+                        </ul>
+                    </div>
                 </li>
                 <li className="link">
                     <FaIndustry />
