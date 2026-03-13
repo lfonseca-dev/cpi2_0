2f90caf6cae0bce49156a817ef1b447c5fe0e7a5
AmorimDeveloper <gabriel.amorim.etec2@gmail.com>
2026-03-13 19:59:11 +0000

    Atualizacao no card de cadastro navBar

--- APP/src/components/NavBar.jsx
+++ APP/src/components/NavBar.jsx
@@ -1,11 +1,15 @@
+import { useState } from "react";
 import { FaIndustry } from "react-icons/fa";
 import { FaMagnifyingGlass, FaCirclePlus } from "react-icons/fa6";
 import Logo from "../assets/LogoFaulin.png";
 
 function NavBar() {
+    const [cadastroOpen, setCadastroOpen] = useState(false);
+
     return ( 
-        <header className="flex items-center justify-center fixed bg-white p-4 h-[13vh] w-screen top-0 shadow">
-            <span className="flex items-center justify-center h-[145%] w-[12%]">
+        <header className="fixed top-0 w-screen shadow h-[13vh] box-border">
+            <nav className="flex items-center justify-center relative bg-white w-full h-full">
+            <span className="flex items-center justify-center h-[90%] w-[12%]">
                 <img src={Logo} alt="" 
                 className="h-full aspect-square object-contain duration-[.3s] hover:scale-[1.2]"/>
             </span>
@@ -14,34 +18,37 @@ function NavBar() {
                     <FaMagnifyingGlass />
                     <span>Consultar</span>
                 </li>
-                <li className="link cadastro h-full">
+                <li className="link cadastro cursor-pointer" onClick={() => setCadastroOpen(open => !open)}>
                     <FaCirclePlus />
                     <span>Cadastro</span>
-                    <div className="flex justify-center absolute bg-neutral-800 h-[270px] bottom-[-270px] text-white p-4">
-                        <ul className="flex flex-col justify-between items-center">
-                            <li className="link">user</li>
-                            <li className="link">categoria</li>
-                            <li className="link">produto</li>
-                            <li className="link">fornecedor</li>
-                            <li className="link">operador</li>
-                            <li className="link">engenheiro</li>
-                            <li className="link">responsavel</li>
-                            <li className="link">destino</li>
-                            <li className="link">local_estoque</li>
-                        </ul>
-                    </div>
                 </li>
                 <li className="link">
                     <FaIndustry />
                     <span>Produção</span>
                 </li>
             </ul>
-            <span className="flex items-center justify-center h-full w-[12%]">
+            <span className="flex items-center justify-center h-[60%] w-[12%]">
                 <img src="https://thumbs.dreamstime.com/b/%C3%ADcone-de-perfil-do-avatar-padr%C3%A3o-vetor-m%C3%ADdia-social-usu%C3%A1rio-retrato-176256935.jpg" alt="" 
                 className="h-full aspect-square object-contain bg-[gray] duration-[.3s] rounded-[50rem] border-[2px] border-[gray] hover:scale-[1.2]"/>
             </span>
+            <div className={`boxCadastro flex justify-center absolute bg-neutral-800 transition-[.4s] bottom-0 z-[-100] overflow-hidden h-[270px] text-white p-4
+            ${cadastroOpen ? "visivel" : ""}`}>
+                <ul className="flex flex-col justify-between items-center text-nowrap">
+                    <li className="link">User</li>
+                    <li className="link">Categoria</li>
+                    <li className="link">Produto</li>
+                    <li className="link">Fornecedor</li>
+                    <li className="link">Operador</li>
+                    <li className="link">Engenheiro</li>
+                    <li className="link">Responsavel</li>
+                    <li className="link">Destino</li>
+                    <li className="link">Local Estoque</li>
+                </ul>
+            </div>
+            </nav>
         </header>
     );
 }
+
 
 export default NavBar;
